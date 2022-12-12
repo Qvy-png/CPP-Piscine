413cee7fe7887f87a679dfd85fe6b80cd4c73b50
Robin DEL AGUILA <delaguilarobin5@gmail.com>
2022-12-12 13:36:01 +0100

    endoftheday

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: rdel-agu <rdel-agu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 13:28:39 by rdel-agu          #+#    #+#             */
-/*   Updated: 2022/12/11 18:32:56 by rdel-agu         ###   ########.fr       */
+/*   Updated: 2022/12/12 13:33:06 by rdel-agu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define ARRAY_HPP
 
 #include <iostream>
+#include "color.hpp"
 
 template <typename T>
 class Array {
@@ -25,14 +26,42 @@ class Array {
         
     public :
     
-        Array( void );
-        Array( unsigned int n );
-        Array( const Array& );
-        ~Array( void );
+        Array( void ) : _array( NULL ), _size( 0 ) { };
+        
+        Array( unsigned int n ) : _array( new T[n] ), _size( n ) { };
+        Array( const Array& ref ) { *this = ref; };
+        ~Array( void ) { delete[]( _array ); };
 
-        operator=( const Array& );
+        Array& operator=( const Array& ref ) {
+        
+            size_t i = 0;
+            
+            _size = ref._size;
+            _array = new T[_size];
+            while ( i < _size ) {
+            
+                _array[i] =  ref._array[i];
+                i++;
+            }
+            return ( *this );
+        }
 
-        T* getArray() const;
+        T&  operator[]( int i ) {
+        
+            if ( i > -1 && static_cast<unsigned int>( i ) < _size )
+                return ( _array[i] );
+            else
+                throw invalidIndex();
+        }
+
+        size_t size() const { return( _size ); };
+        
+        class invalidIndex : public std::exception {
+        
+            public:
+
+                virtual const char* what() const throw() { return ( REDHB "Invalid index" CRESET ); };
+        };
 };
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,20 +6,80 @@
 /*   By: rdel-agu <rdel-agu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 13:28:05 by rdel-agu          #+#    #+#             */
-/*   Updated: 2022/12/11 18:40:21 by rdel-agu         ###   ########.fr       */
+/*   Updated: 2022/12/12 13:33:41 by rdel-agu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
-
-Array::Array( void ) : _array( NULL ), _size( 0 ) {
-
-    
-}
+#include <cstdlib>
 
 int main( void ) {
 
-    int *a = new int();
+    unsigned int size = 10;
+    unsigned int i = 0;
 
-    std::cout << *a << std::endl;
+    Array<int>   empty( 0 );
+    Array<int>   full( size );
+    int*         tab = new int[size];
+    srand(time(NULL));
+
+    //////////////////////////////////////
+    //                                  //
+    //     operateur d'assignation      //
+    //                                  //
+    //////////////////////////////////////
+
+    while ( i < size ) {
+    
+        const int value = rand();
+        full[i] = value;
+        tab[i] = value;
+        i++;
+    }
+    
+    Array<int> tmp = full;
+
+    i = 0;
+    while ( i < size ) {
+    
+        std::cout << "Array[" << i << "] = " << GRN << full[i] << CRESET ", Tab[" << i << "] = " << GRN << tab[i] << CRESET << std::endl;
+        i++;
+    }
+	
+    //////////////////////////////////////
+    //                                  //
+    //           exceptions             //
+    //                                  //
+    //////////////////////////////////////
+    
+    try
+    {
+        full[-23] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+	
+    try
+    {
+        full[size] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+	try
+    {
+        full[100000] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    delete [] tab;
+    return 0;
 }
+
